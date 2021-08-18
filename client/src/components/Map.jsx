33376a73478e62b0730ddef55f42f1e241616de3
zongxilli <zongxi2014@gmail.com>
2021-08-17 22:30:01 -0400

    added map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,42 +1,75 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-// import Button from "@material-ui/core/Button";
-// import Typography from "@material-ui/core/Typography";
-// import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+import React, { useState } from 'react';
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import DeleteIcon from '@material-ui/icons/DeleteTwoTone';
+import Brightness6Icon from '@material-ui/icons/Brightness6';
+
+const INITIAL_VIEWPORT = {
+	latitude: 43.6532,
+	longitude: -79.3832,
+	zoom: 13,
+};
+
+// Kenny Addition
+const INITIAL_MAP = 'mapbox://styles/mapbox/navigation-night-v1';
 
 const Map = ({ classes }) => {
-  return <div >Map</div>;
+	// Kenny Addition
+	const [currMap, setCurrMap] = useState(INITIAL_MAP);
+
+	const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+
+	return (
+		<div className={classes.root}>
+			<ReactMapGL
+				width="100vw"
+				height="calc(100vh - 64px)"
+				mapStyle={currMap}
+				mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API_ACCESS_TOKEN}
+				onViewportChange={(currViewport) => setViewport(currViewport)}
+				{...viewport}>
+				{/* Navigation Control tool bar */}
+				<div className={classes.navigationControl}>
+					<NavigationControl
+						onViewportChange={(currViewport) => setViewport(currViewport)}
+					/>
+				</div>
+			</ReactMapGL>
+		</div>
+	);
 };
 
 const styles = {
-  root: {
-    display: "flex"
-  },
-  rootMobile: {
-    display: "flex",
-    flexDirection: "column-reverse"
-  },
-  navigationControl: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    margin: "1em"
-  },
-  deleteIcon: {
-    color: "red"
-  },
-  popupImage: {
-    padding: "0.4em",
-    height: 200,
-    width: 200,
-    objectFit: "cover"
-  },
-  popupTab: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column"
-  }
+	root: {
+		display: 'flex',
+	},
+	rootMobile: {
+		display: 'flex',
+		flexDirection: 'column-reverse',
+	},
+	navigationControl: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		margin: '1em',
+	},
+	deleteIcon: {
+		color: 'red',
+	},
+	popupImage: {
+		padding: '0.4em',
+		height: 200,
+		width: 200,
+		objectFit: 'cover',
+	},
+	popupTab: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		flexDirection: 'column',
+	},
 };
 
 export default withStyles(styles)(Map);
