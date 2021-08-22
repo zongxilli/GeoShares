0b2ea917cb134c5f7847b6f25d21c9ba6ea344c6
zongxilli <zongxi2014@gmail.com>
2021-08-22 00:44:45 -0400

    29 finished

--- client/src/components/Comment/CreateComment.jsx
+++ client/src/components/Comment/CreateComment.jsx
@@ -1,32 +1,57 @@
-import React from "react";
-import { withStyles } from "@material-ui/core";
-// import InputBase from "@material-ui/core/InputBase";
-// import IconButton from "@material-ui/core/IconButton";
-// import ClearIcon from "@material-ui/icons/Clear";
-// import SendIcon from "@material-ui/icons/Send";
-// import Divider from "@material-ui/core/Divider";
+import React from 'react';
+import { withStyles } from '@material-ui/core';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import ClearIcon from '@material-ui/icons/Clear';
+import SendIcon from '@material-ui/icons/Send';
+import Divider from '@material-ui/core/Divider';
 
 const CreateComment = ({ classes }) => {
-  return <div>CreateComment</div>;
+	return (
+		<>
+			<form className={classes.form}>
+				<IconButton
+					//onClick={() => setComment('')}
+					//disabled={!comment.trim()}
+					className={classes.clearButton}>
+					<ClearIcon />
+				</IconButton>
+				<InputBase
+					className={classes.input}
+					placeholder="Add Comment"
+					
+					//value={comment}
+					//onChange={(e) => setComment(e.target.value)}
+				/>
+				<IconButton
+					//onClick={handleSubmitComment}
+					//disabled={!comment.trim()}
+					className={classes.sendButton}>
+					<SendIcon />
+				</IconButton>
+			</form>
+			<Divider />
+		</>
+	);
 };
 
-const styles = theme => ({
-  form: {
-    display: "flex",
-    alignItems: "center"
-  },
-  input: {
-    marginLeft: 8,
-    flex: 1
-  },
-  clearButton: {
-    padding: 0,
-    color: "red"
-  },
-  sendButton: {
-    padding: 0,
-    color: theme.palette.secondary.dark
-  }
+const styles = (theme) => ({
+	form: {
+		display: 'flex',
+		alignItems: 'center',
+	},
+	input: {
+		marginLeft: 8,
+		flex: 1,
+	},
+	clearButton: {
+		padding: 0,
+		color: 'red',
+	},
+	sendButton: {
+		padding: 0,
+		color: theme.palette.secondary.dark,
+	},
 });
 
 export default withStyles(styles)(CreateComment);
--- client/src/components/Pin/PinContent.jsx
+++ client/src/components/Pin/PinContent.jsx
@@ -6,6 +6,8 @@ import FaceIcon from '@material-ui/icons/Face';
 import format from 'date-fns/format';
 
 import Context from '../../context';
+import Comments from '../Comment/Comments';
+import CreateComment from '../Comment/CreateComment';
 
 const PinContent = ({ classes }) => {
 	const { state } = useContext(Context);
@@ -35,6 +37,10 @@ const PinContent = ({ classes }) => {
 			<Typography variant="subtitle1" gutterBottom>
 				{content}
 			</Typography>
+
+			{/* Pin Comments */}
+			<CreateComment />
+			<Comments comments={comments} />
 		</div>
 	);
 };
